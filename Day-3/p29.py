b55cc6c18a67d8a258facdd179cd0a1e2927fcba
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-01-16 12:31:44 +0600

    Delete Day-3 directory

--- Day-3/p29.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to boat journey.")
-statement = input("Do you know about how to swim?(write yes or no)\n")
-if statement == 'yes':
-  print("You are eligible for the boat journey.")
-else:
-  print("You are not eligible for the journey.")
--- Day-3/p30.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter your number: "))
-if num % 2 == 0:
-  print("This is a Even number.")
-else:
-  print("This is an odd number.")
