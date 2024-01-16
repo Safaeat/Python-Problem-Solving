3b38b5d5da11535c9bfb977d4f8a8f5a545a0cce
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-01-16 12:35:26 +0600

    Delete p13.py

--- p13.py
+++
@@ -1,9 +0,0 @@
-#define by age
-age = input("Enter the age:")
-
-if int(age) <= 18:
-    print("You can not give the vot.")
-elif int(age) >= 18:
-    print("You are allow to give the vot.")
-else:
-    print("Please an integer number.")
