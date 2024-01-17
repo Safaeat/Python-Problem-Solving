9bcc9750f20cf7246cb89e56452dc9998f70e0e7
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-01-17 22:37:25 +0600

    Add files via upload

---
+++ 100-Days python/Day-4/p40(random).py
@@ -0,0 +1,10 @@
+import random
+
+random_int = random.randint(1, 10)
+print(random_int)
+
+random_float = random.random()*100
+print(random_float)
+
+love_calculator = random.randint(0, 100)
+print(f"Your love score is {love_calculator}.")
