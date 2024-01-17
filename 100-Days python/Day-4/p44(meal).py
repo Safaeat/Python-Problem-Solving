80670fc043919868dc8d42cdd7d8e69d85058cdd
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-01-18 02:36:46 +0600

    Add files via upload

---
+++ 100-Days python/Day-4/p44(meal).py
@@ -0,0 +1,9 @@
+import random
+name = input("Input tha names(Ex: rana, mina, tina): ")
+
+list = name.split(", ")
+member = len(list)
+print(f"Total number of member is {member}.")
+print(list)
+index = random.randint(0, member-1)
+print(f"{list[index]} is going to buy meal today!")
