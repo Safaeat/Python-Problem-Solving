7b97a4e6a375db4044181f1ff470b2a4320ee545
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-01-19 22:38:48 +0600

    Delete p27.py

--- p27.py
+++
@@ -1,8 +0,0 @@
-age = int(input("What is your current age?"))
-
-years = int(90-age)
-month = int(years * 12)
-week = int(years * 52)
-day = 365 * years
-
-print(f"You will gona leave {day} days or {week} weeks or {month} months or {years} years")
