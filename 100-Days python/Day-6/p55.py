029cbf1e6e41237b4685afc44de7cf02bff5ecf2
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-03-03 20:53:58 +0600

    Add files via upload

---
+++ 100-Days python/Day-6/p55.py
@@ -0,0 +1,12 @@
+def name():
+ print("My name is Safaeat.")
+ print("I am from Dhaka.")
+
+def hobby():
+ print("My favourite hobby is playing Soccer.")
+
+n = input(f"input 0 or other numbers to know about me!\n")
+if n == "0":
+ name()
+else:
+ hobby()
