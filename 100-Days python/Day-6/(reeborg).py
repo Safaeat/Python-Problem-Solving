343fc45ba58715ac52448ba8aba5173cf6aa9608
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-03-03 21:42:00 +0600

    Add files via upload

---
+++ 100-Days python/Day-6/(reeborg).py
@@ -0,0 +1,30 @@
+def mov():
+    move()
+    move()
+    move()
+def turn_back():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def around():
+    turn_left()
+    move()
+    move()
+    move()
+    
+around()
+turn_right()
+mov()
+turn_right()
+mov()
+turn_right()
+mov()
+turn_back()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
