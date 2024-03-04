2d4c65e6c35b5009457a7265f1255175b502372a
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-03-04 18:43:02 +0600

    Add files via upload

---
+++ 100-Days python/Day-6/(Reeborg's world game).py
@@ -0,0 +1,33 @@
+def mov():
+    move()
+    build_wall()
+    turn_left()
+    move()
+    turn_right()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+
+mov()
+turn_right()
+turn_left()
+mov()
+turn_right()
+turn_left()
+mov()
+turn_right()
+turn_left()
+mov()
+turn_right()
+turn_left()
+move()
+done()
+        
+        
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
