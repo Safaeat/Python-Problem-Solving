9300d8b20939bfbc59d20afae39e0505c1c8ba5a
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-03-10 20:46:21 +0600

    Add files via upload

---
+++ 100-Days python/Day-7/p57.py
@@ -0,0 +1,10 @@
+list1 = ['apple','banana','orange','malta','kola','lichu']
+list2 = ['papa','mango']
+
+list2.insert(0,'banggi')
+fruits = list1 + list2
+print(f"All fruit list : {fruits}")
+
+list1[3:-1] = 'Anar'
+print(f"All fruit list from list 1 : {list1}")
+
