8ae33262255a88bf7110a29845c1115cc6bfe2c1
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-03-10 23:53:23 +0600

    Add files via upload

--- 100-Days python/Day-7/p60.py
+++ 100-Days python/Day-7/p60.py
@@ -1,8 +1,60 @@
 import random
 
+stage = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
 list = []
 list1 = ['apple','banana','orange','malta','kola','lichu']
 count = 0
+life = 0
 word = random.choice(list1)
 print(f"The ans is : "+word)
 length = len(word)
@@ -14,6 +66,9 @@ while count < length:
 end_of_game = False
 while not end_of_game:
   guess = input("Guess the word: ")
+  if guess not in word:
+     life += 1
+     print(stage[life])
 
   for index in range(length):
     if guess == word[index]:
@@ -25,4 +80,11 @@ while not end_of_game:
   if '_' not in list:
      end_of_game = True
      print("You win.")
+
+  if life == 6:
+     end_of_game = True
+     print("You lose.")
+
+
+
 print(list)
