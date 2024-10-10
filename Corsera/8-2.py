99ded7a464b436027837de4c86245e25752c20a0
Safaeat <89898173+Safaeat@users.noreply.github.com>
2024-10-11 02:48:42 +0600

    Create 8-2.py

---
+++ Corsera/8-2.py
@@ -0,0 +1,9 @@
+numlist = list()
+while True:
+value = input("Enter the number: ")
+if value = "done" : break
+n = float(value)
+numlist.append(n)
+
+average = sum(numlist)/len(numlist)
+print("The average value is: ", average)
